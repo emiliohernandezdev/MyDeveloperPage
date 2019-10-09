dfe2c5272d67b4d0fe3a5f5789f8951098f38828
Emilio Andres Hernandez <emiliohernandezgt@gmail.com>
2019-10-09 15:17:51 -0600

    Firebase released version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    children: [
+      {
+        path: '',
+        loadChildren: './components/body/body.module#BodyModule'
+      },
+      {
+        path: 'about',
+        loadChildren: './components/about-me/about-me.module#AboutMeModule'
+      },
+      {
+        path: 'services',
+        loadChildren: './components/services/services.module#ServicesModule'
+      },
+      {
+        path: '**',
+        loadChildren: './components/not-found/not-found.module#NotFoundModule'
+      }
+    ]
+  },
+  {
+    path: '**',
+    loadChildren: './components/not-found/not-found.module#NotFoundModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutMeModule } from './components/about-me/about-me.module';
+import { ServicesModule } from './components/services/services.module';
+import { NotFoundModule } from './components/not-found/not-found.module';
+import { BodyModule } from './components/body/body.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AboutMeModule,
+    ServicesModule,
+    NotFoundModule,
+    BodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
